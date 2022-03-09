178017a0ff4afd19dec1ba7c81a12c7882ab547b
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-03-09 14:37:34 +0100

    Practice with expresions, numbers and operators

--- js-practice.js
+++ js-practice.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+console.log("Hello World!");
+/* This is a repository with exercise about JavaScript */
+
+// Basic data types: Numbers
+let cheeseCalories = 200;
+let eggsRequired = 4;
+let caloriesPerEgg = 75;
+let eggsCalories = eggsRequired * caloriesPerEgg;
+console.log(eggsCalories);
+
+let omeletteDuFromageCalories = cheeseCalories + eggsCalories;
+console.log(omeletteDuFromageCalories);
+console.log (omeletteDuFromageCalories / 2);
+
+const i = 10 + (5 * 2 ** 3) / 4 - 6;
+console.log(i);
+
+let myAge = 34;
+myAge += 1;
+console.log(myAge);
